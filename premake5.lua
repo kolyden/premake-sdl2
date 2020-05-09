2cf1fdf3e330405357dd5c35b7cab2c921d7aafe
Denis <kolyden@gmail.com>
2020-05-09 18:39:16 +0200

    windows update

---
+++ premake5.lua
@@ -0,0 +1,207 @@
+SDL2_DIR     = "sdl2"
+SDL2_INCLUDE = SDL2_DIR.."/include"
+
+workspace "sdl2"
+    targetdir "bin/%{_ACTION}-%{cfg.platform}-%{cfg.buildcfg}/%{prj.name}"
+    objdir "temp/%{_ACTION}-%{cfg.platform}-%{cfg.buildcfg}/%{prj.name}"
+    location (path.join("project", _ACTION))
+    configurations { "Release", "Debug" }
+
+project "sdl2"
+    kind "StaticLib"
+    language "C"
+    defines "HAVE_LIBC"
+    includedirs { 
+        path.join(SDL2_DIR, "include"),
+    }
+    files {
+        path.join(SDL2_DIR, "include/**"),
+        path.join(SDL2_DIR, "src/*.c"),
+        path.join(SDL2_DIR, "src/*.h"),
+        path.join(SDL2_DIR, "src/cpuinfo/**"),
+        path.join(SDL2_DIR, "src/dynapi/**"),
+        path.join(SDL2_DIR, "src/atomic/**"),
+        path.join(SDL2_DIR, "src/libm/**"),
+        path.join(SDL2_DIR, "src/stdlib/**"),
+        path.join(SDL2_DIR, "src/filesystem/dummy/**"),
+        path.join(SDL2_DIR, "src/loadso/dummy/**"),
+        -- AUDIO --
+        path.join(SDL2_DIR, "src/audio/*.c"),
+        path.join(SDL2_DIR, "src/audio/*.h"),
+        path.join(SDL2_DIR, "src/audio/dummy/**"),
+        -- EVENTS --
+        path.join(SDL2_DIR, "src/events/SDL_*"),
+        path.join(SDL2_DIR, "src/events/*_cursor.h"),
+        -- FILE --
+        path.join(SDL2_DIR, "src/file/*.c"),
+        -- HAPTIC --
+        path.join(SDL2_DIR, "src/haptic/*.c"),
+        path.join(SDL2_DIR, "src/haptic/*.h"),
+        path.join(SDL2_DIR, "src/haptic/dummy/**"),
+        -- joystick --
+        path.join(SDL2_DIR, "src/joystick/*.c"),
+        path.join(SDL2_DIR, "src/joystick/*.h"),
+        path.join(SDL2_DIR, "src/joystick/dummy/**"),
+        path.join(SDL2_DIR, "src/joystick/hidapi/*.c"),
+        path.join(SDL2_DIR, "src/joystick/hidapi/*.h"),
+        path.join(SDL2_DIR, "src/joystick/virtual/*"),
+        -- LOCALE --
+        path.join(SDL2_DIR, "src/locale/*.c"),
+        path.join(SDL2_DIR, "src/locale/*.h"),
+        path.join(SDL2_DIR, "src/locale/dummy/**"),
+        -- POWER --
+        path.join(SDL2_DIR, "src/power/*.c"),
+        path.join(SDL2_DIR, "src/power/*.h"),
+        -- RENDER --
+        path.join(SDL2_DIR, "src/render/*.c"),
+        path.join(SDL2_DIR, "src/render/*.h"),
+        path.join(SDL2_DIR, "src/render/software/**"),
+        path.join(SDL2_DIR, "src/render/opengl/**"),
+        path.join(SDL2_DIR, "src/render/opengles2/**"),
+        -- SENSOR --
+        path.join(SDL2_DIR, "src/sensor/*.c"),
+        path.join(SDL2_DIR, "src/sensor/*.h"),
+        path.join(SDL2_DIR, "src/sensor/dummy/**"),
+        -- THREAD --
+        path.join(SDL2_DIR, "src/thread/*.c"),
+        path.join(SDL2_DIR, "src/thread/*.h"),
+        -- TIMER --
+        path.join(SDL2_DIR, "src/timer/*.c"),
+        path.join(SDL2_DIR, "src/timer/*.h"),
+        path.join(SDL2_DIR, "src/timer/dummy/**"),
+        -- VIDEO --
+        path.join(SDL2_DIR, "src/video/*.c"),
+        path.join(SDL2_DIR, "src/video/*.h"),
+        path.join(SDL2_DIR, "src/video/dummy/**"),
+        path.join(SDL2_DIR, "src/video/yuv2rgb/**"),
+    }
+
+    filter "system:windows"
+        links { "setupapi", "winmm", "imm32", "version" }
+        files {
+            path.join(SDL2_DIR, "src/audio/directsound/**"),
+            path.join(SDL2_DIR, "src/audio/disk/**"),
+            path.join(SDL2_DIR, "src/audio/winmm/**"),
+            path.join(SDL2_DIR, "src/audio/wasapi/**"),
+            path.join(SDL2_DIR, "src/core/windows/**"),
+            path.join(SDL2_DIR, "src/events/scancodes_windows.h"),
+            path.join(SDL2_DIR, "src/filesystem/windows/**"),
+            path.join(SDL2_DIR, "src/haptic/windows/**"),
+            path.join(SDL2_DIR, "src/hidapi/windows/hid.c"),
+            path.join(SDL2_DIR, "src/joystick/windows/**"),
+            path.join(SDL2_DIR, "src/loadso/windows/**"),
+            path.join(SDL2_DIR, "src/locale/windows/**"),
+            path.join(SDL2_DIR, "src/power/windows/**"),
+            path.join(SDL2_DIR, "src/render/direct3d/**"),
+            path.join(SDL2_DIR, "src/render/direct3d11/**"),
+            path.join(SDL2_DIR, "src/sensor/windows/**"),
+            path.join(SDL2_DIR, "src/thread/generic/SDL_syscond.c"),
+            path.join(SDL2_DIR, "src/thread/windows/**"),
+            path.join(SDL2_DIR, "src/timer/windows/**"),
+            path.join(SDL2_DIR, "src/video/windows/**"),
+        }
+        removefiles {
+            "**/SDL_render_winrt.*"
+        }
+        defines {
+            "SDL_DISABLE_WINDOWS_IME",
+            "WIN32",
+            "__WIN32__",
+            --"SDL_SHARED",
+            "VC_EXTRALEAN",
+            "_CRT_SECURE_NO_WARNINGS",
+        }
+
+        links { "user32", "gdi32", "winmm", "imm32", "ole32", "oleaut32", "version", "uuid" }
+
+    filter "system:linux"
+        files {
+            path.join(SDL2_DIR, "src/core/windows/**"),
+            path.join(SDL2_DIR, "src/events/scancodes_linux.h"),
+            path.join(SDL2_DIR, "src/haptic/linux/**"),
+            path.join(SDL2_DIR, "src/joystick/linux/**"),
+            path.join(SDL2_DIR, "src/locale/unix/**"),
+            path.join(SDL2_DIR, "src/power/linux/**"),
+        }
+
+    filter "system:macosx"
+        files {
+            path.join(SDL2_DIR, "src/events/scancodes_darwin.h"),
+            path.join(SDL2_DIR, "src/file/cocoa/**"),
+            path.join(SDL2_DIR, "src/filesystem/cocoa/**"),
+            path.join(SDL2_DIR, "src/haptic/darwin/**"),
+            path.join(SDL2_DIR, "src/joystick/darwin/**"),
+            path.join(SDL2_DIR, "src/locale/macosx/**"),
+            path.join(SDL2_DIR, "src/power/macosx/**"),
+            path.join(SDL2_DIR, "src/video/cocoa/**"),
+        }
+        defines {
+            --"SDL_SHARED",
+            "SDL_POWER_MACOSX",
+            "SDL_VIDEO_DRIVER_COCOA",
+            "SDL_VIDEO_OPENGL_GLX",
+            "SDL_LOADSO_DLOPEN",
+            "SDL_TIMER_UNIX",
+            "SDL_FRAMEWORK_COCOA",
+            "SDL_FRAMEWORK_CARBON",
+            "SDL_FILESYSTEM_COCOA",
+        }
+        buildoptions {"-fPIC"}
+        links {
+            "ForceFeedback.framework",
+            "CoreVideo.framework",
+            "Cocoa.framework",
+            "IOKit.framework",
+            "Carbon.framework",
+            "CoreAudio.framework",
+            "AudioToolbox.framework",
+            "dl"
+        }
+
+    filter "system:android"
+        files {
+            path.join(SDL2_DIR, "src/audio/android/**"),
+            path.join(SDL2_DIR, "src/audio/openslES/**"),
+            path.join(SDL2_DIR, "src/core/android/**"),
+            path.join(SDL2_DIR, "src/filesystem/android/**"),
+            path.join(SDL2_DIR, "src/haptic/android/**"),
+            path.join(SDL2_DIR, "src/hidapi/android/hid.cpp"),
+            path.join(SDL2_DIR, "src/joystick/android/**"),
+            path.join(SDL2_DIR, "src/locale/android/**"),
+            path.join(SDL2_DIR, "src/power/android/**"),
+            path.join(SDL2_DIR, "src/sensor/android/**"),
+            path.join(SDL2_DIR, "src/video/android/**"),
+
+            path.join(SDL2_DIR, "src/loadso/dlopen/**"),
+            path.join(SDL2_DIR, "src/thread/pthread/**"),
+            path.join(SDL2_DIR, "src/timer/unix/**"),
+            path.join(SDL2_DIR, "src/render/opengles/**"),
+        }
+        
+    filter "system:ios"
+        files
+        {
+            path.join(SDL2_DIR, "src/joystick/iphoneos/**"),
+        }
+
+    filter "action:vs*"
+        defines "_CRT_SECURE_NO_WARNINGS"
+
+project "sdl2_test_common"
+    kind "StaticLib"
+    files { path.join(SDL2_DIR, "src/test/**") }
+    includedirs { SDL2_INCLUDE }
+
+project "sdl2_main"
+    kind "StaticLib"
+    files { SDL2_DIR.."src/main/windows/*.c" }
+    includedirs { SDL2_INCLUDE }
+
+SDL2_TEST_DIR = path.join(SDL2_DIR, "test")
+
+project "Test"
+    kind "WindowedApp"
+    links { "sdl2", "sdl2_main", "sdl2_test_common" }
+    files { path.join(SDL2_DIR,"test/testsprite2.c") }
+    includedirs { SDL2_INCLUDE }
+    debugdir (SDL2_TEST_DIR)
